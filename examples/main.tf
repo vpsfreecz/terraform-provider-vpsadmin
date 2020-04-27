e557e815b503467e6a6df5cebd80b462cbfd3e6d
Jakub Skokan <jakub.skokan@havefun.cz>
2020-04-27 09:43:01 +0200

    examples/main: only one pubkey per file is accepted

--- examples/main.tf
+++ examples/main.tf
@@ -11,7 +11,7 @@ provider "vpsadmin" {
 resource "vpsadmin_ssh_key" "my-key" {
   label = "My public key"
 
-  # Set your public key here
+  # Set your public key here. The file has to contain exactly one public key.
   key = file("~/.ssh/my_key.pub")
 }
 
