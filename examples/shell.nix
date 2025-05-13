bb249a55783b6797a49caffb2cd6a53ee98cfe8e
Jakub Skokan <jakub.skokan@havefun.cz>
2025-05-13 13:59:03 +0200

    examples: use opentofu and update get-token

--- examples/shell.nix
+++ examples/shell.nix
@@ -4,7 +4,7 @@ let
   get-token = pkgs.buildGoModule {
     name = "get-token";
     src = ../get-token;
-    vendorSha256 = "sha256:0dpjll0qhkgq6yidz2p7451l35z92c60a2dggk2vq7x54w0ksi3l";
+    vendorHash = "sha256-TOqys10Q1BEEGhBHIppemjMP5iT0HFQbcAEgqh0AEyw=";
   };
 
 in stdenv.mkDerivation rec {
@@ -12,6 +12,6 @@ in stdenv.mkDerivation rec {
 
   buildInputs = with pkgs; [
     get-token
-    terraform
+    opentofu
   ];
 }
